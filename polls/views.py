f236458c4d6b16fa9d3703f05b5d8e250bbff033
Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>
2022-11-26 18:48:33 +0330

    Using shortcut for not found page
    Signed-off-by: Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 from django.template import loader
 from .models import Question
@@ -10,10 +10,13 @@ def index(request):
     return render(request,'polls\index.html',context)
 
 def detail(request,question_id):
+    """ 
     try:
         q = Question.objects.get(pk = question_id)
     except Question.DoesNotExist:
         raise Http404("Question Does not exist")
+        """
+    q = get_object_or_404(Question,pk=question_id)
     return render(request, 'polls\details.html',{'question':q})    
 
 def results(request, question_id):
