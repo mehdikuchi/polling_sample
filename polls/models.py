cefc88b23ebb8c756c39a88b0cabd623a394675e
Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>
2022-11-30 21:42:15 +0330

    Fixing the bug and complete the unit test for the publication time
    Signed-off-by: Mahdi Tanbakuchi <mehdi.kuchi@gmail.com>

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     def __str__(self) -> str:
         return self.QuestionText
     def was_published_recently(self):
-        return self.pub_date>= timezone.now()- datetime.timedelta(days=1)
+        return timezone.now()>=self.pub_date>= timezone.now()- datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
